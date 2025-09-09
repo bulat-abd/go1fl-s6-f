3f4f4ec7b62100b84d0e572ce86ef52ddc171f77
Bulat <babdullatypov@gmail.com>
2025-09-09 12:42:19 +0300

    Fix main page GET handler

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,10 +3,10 @@ package handlers
 import "net/http"
 
 
-func mainHandle(w http.ResponseWriter, req *http.Request) {
+func MainHandler(w http.ResponseWriter, req *http.Request) {
     if req.Method != http.MethodGet {
         http.Error(w, "Сервер не поддерживает "+req.Method, http.StatusMethodNotAllowed)
         return
     }
-    http.ServeFile(w, r, "index.html")
+    http.ServeFile(w, req, "../index.html")
 }
