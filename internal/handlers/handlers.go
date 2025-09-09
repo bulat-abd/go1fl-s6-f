1613fa98b0d5988cabbbc2bdaf29d130522f545b
Bulat <babdullatypov@gmail.com>
2025-09-09 11:18:37 +0300

    Add main page GET handler

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,12 @@
 package handlers
+
+import "net/http"
+
+
+func mainHandle(w http.ResponseWriter, req *http.Request) {
+    if req.Method != http.MethodGet {
+        http.Error(w, "Сервер не поддерживает "+req.Method, http.StatusMethodNotAllowed)
+        return
+    }
+    http.ServeFile(w, r, "index.html")
+}
